d63c940d4668b77f98cba3852c2b6ced1ca18b6e
김준환 <musoyou1085@gmail.com>
2025-08-17 15:33:17 +0900

    :sparkles: feature: 시간 자동 생성 로직

--- src/main/kotlin/simplerag/ragback/RagBackApplication.kt
+++ src/main/kotlin/simplerag/ragback/RagBackApplication.kt
@@ -2,8 +2,10 @@ package simplerag.ragback
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class RagBackApplication
 
 fun main(args: Array<String>) {
