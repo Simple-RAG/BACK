625a91e2a00090400464e8b3fa00f31dd50dc20b
김준환 <musoyou1085@gmail.com>
2025-08-17 16:28:37 +0900

    :bug: fix: size check 로직 추가

--- src/main/kotlin/simplerag/ragback/domain/index/entity/Index.kt
+++ src/main/kotlin/simplerag/ragback/domain/index/entity/Index.kt
@@ -1,6 +1,7 @@
 package simplerag.ragback.domain.index.entity
 
 import jakarta.persistence.*
+import jakarta.validation.constraints.Min
 import simplerag.ragback.domain.index.entity.enums.SimilarityMetric
 import simplerag.ragback.global.entity.BaseEntity
 
@@ -12,15 +13,18 @@ class Index(
     val snapshotName: String,
 
     @Column(name = "chunking_size", nullable = false)
+    @Min(1)
     val chunkingSize: Int,
 
     @Column(name = "overlap_size", nullable = false)
+    @Min(0)
     val overlapSize: Int,
 
     @Column(name = "similarity_metric", nullable = false)
     val similarityMetric: SimilarityMetric,
 
     @Column(name = "top_k", nullable = false)
+    @Min(1)
     val topK: Int,
 
     @Column(name = "embedding_model", nullable = false, length = 255)
