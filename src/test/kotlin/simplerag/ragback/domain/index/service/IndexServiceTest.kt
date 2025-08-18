e70fe40987b75291b6c2ef4edcb6030a7545a153
김준환 <musoyou1085@gmail.com>
2025-08-18 14:52:34 +0900

    :test_tube: test: 인덱스 생성 테스트 코드 추가

---
+++ src/test/kotlin/simplerag/ragback/domain/index/service/IndexServiceTest.kt
@@ -0,0 +1,59 @@
+package simplerag.ragback.domain.index.service
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import simplerag.ragback.domain.index.dto.IndexCreateRequest
+import simplerag.ragback.domain.index.entity.enums.EmbeddingModel
+import simplerag.ragback.domain.index.entity.enums.SimilarityMetric
+import simplerag.ragback.domain.index.repository.IndexRepository
+import simplerag.ragback.global.error.IndexException
+
+@SpringBootTest
+@ActiveProfiles("test")
+class IndexServiceTest (
+    @Autowired private val indexRepository: IndexRepository,
+    @Autowired private val indexService: IndexService,
+) {
+
+    @AfterEach
+    fun cleanUp() {
+        indexRepository.deleteAll()
+    }
+
+    @Test
+    @DisplayName("인덱스 생성이 정상 작동한다")
+    fun createIndexTest() {
+        // given
+        val indexCreateRequest = IndexCreateRequest("test", 1, 0, SimilarityMetric.COSINE, 1, EmbeddingModel.TEXT_EMBEDDING_3_LARGE, true)
+
+        // when
+        val createIndexResponse = indexService.createIndex(indexCreateRequest);
+
+        // then
+        val indices = indexRepository.findAll()
+        val index = indices[0]
+        assertEquals(index.id, createIndexResponse.indexId)
+        assertEquals(index.snapshotName, createIndexResponse.snapshotName)
+    }
+
+    @Test
+    @DisplayName("인덱스 생성 시 overlap 크기가 chunking 크기를 넘어가면 에러가 터진다")
+    fun getIndexesTest() {
+        // given
+        val indexCreateRequest = IndexCreateRequest("test", 1, 1, SimilarityMetric.COSINE, 1, EmbeddingModel.TEXT_EMBEDDING_3_LARGE, true)
+
+        // when * then
+        val message = assertThrows<IndexException> {
+            indexService.createIndex(indexCreateRequest)
+        }.message
+
+        assertEquals(message, "overlap 크기는 chunking 크기를 넘을 수 없습니다.")
+    }
+
+}
