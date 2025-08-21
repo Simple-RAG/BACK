8c16acac600eb1b6392471094fc41b78d1c58687
김준환 <musoyou1085@gmail.com>
2025-08-21 16:59:41 +0900

    ⚡️ Perf: add EntityGraph in DataFileTagRepository

--- src/main/kotlin/simplerag/ragback/domain/document/repository/DataFileTagRepository.kt
+++ src/main/kotlin/simplerag/ragback/domain/document/repository/DataFileTagRepository.kt
@@ -1,5 +1,6 @@
 package simplerag.ragback.domain.document.repository
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import simplerag.ragback.domain.document.entity.DataFile
 import simplerag.ragback.domain.document.entity.DataFileTag
@@ -7,6 +8,7 @@ import simplerag.ragback.domain.document.entity.DataFileTag
 interface DataFileTagRepository : JpaRepository<DataFileTag, Long> {
     fun existsByDataFileIdAndTagId(dataFileId: Long, tagId: Long): Boolean
 
+    @EntityGraph(attributePaths = ["tag"])
     fun findAllByDataFileIn(dataFiles: Collection<DataFile>): List<DataFileTag>
 
     fun deleteAllByDataFile(dataFile: DataFile)
