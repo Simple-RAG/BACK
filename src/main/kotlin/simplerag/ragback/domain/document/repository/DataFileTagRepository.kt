d5bc90161f7ddd6adf6376cbb9fb56f7e956fc9a
김준환 <musoyou1085@gmail.com>
2025-08-21 16:33:56 +0900

    ♻️ Refactor: index, dataFile converting

--- src/main/kotlin/simplerag/ragback/domain/document/repository/DataFileTagRepository.kt
+++ src/main/kotlin/simplerag/ragback/domain/document/repository/DataFileTagRepository.kt
@@ -1,23 +1,13 @@
 package simplerag.ragback.domain.document.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import simplerag.ragback.domain.document.entity.DataFile
 import simplerag.ragback.domain.document.entity.DataFileTag
 
 interface DataFileTagRepository : JpaRepository<DataFileTag, Long> {
     fun existsByDataFileIdAndTagId(dataFileId: Long, tagId: Long): Boolean
 
-    @Query(
-        """
-        SELECT dft
-        FROM DataFileTag dft
-        JOIN FETCH dft.tag t
-        WHERE dft.dataFile = :dataFile
-    """
-    )
-    fun findTagsByDataFile(@Param("dataFile") dataFile: DataFile): List<DataFileTag>
+    fun findAllByDataFileIn(dataFiles: Collection<DataFile>): List<DataFileTag>
 
     fun deleteAllByDataFile(dataFile: DataFile)
 }
--- src/main/kotlin/simplerag/ragback/domain/index/dto/IndexResponseDTO.kt
+++ src/main/kotlin/simplerag/ragback/domain/index/dto/IndexResponseDTO.kt
@@ -1,16 +1,35 @@
 package simplerag.ragback.domain.index.dto
 
+import simplerag.ragback.domain.index.entity.Index
 import simplerag.ragback.domain.index.entity.enums.EmbeddingModel
 import simplerag.ragback.domain.index.entity.enums.SimilarityMetric
 
 data class IndexPreviewResponseList(
     val indexDetailResponse: List<IndexPreviewResponse>,
-)
+) {
+    companion object {
+        fun toIndexPreviewResponseList(
+            indexes: List<Index>
+        ): IndexPreviewResponseList {
+            val indexList = indexes.map { IndexPreviewResponse.toIndexPreviewResponse(it) }
+            return IndexPreviewResponseList(indexList)
+        }
+    }
+}
 
 data class IndexPreviewResponse(
     var indexId: Long?,
     val snapshotName: String
-)
+) {
+    companion object {
+        fun toIndexPreviewResponse(index: Index): IndexPreviewResponse {
+            return IndexPreviewResponse(
+                indexId = index.id,
+                snapshotName = index.snapshotName,
+            )
+        }
+    }
+}
 
 data class IndexDetailResponse(
     var indexId: Long?,
@@ -21,4 +40,19 @@ data class IndexDetailResponse(
     val topK: Int,
     val embeddingModel: EmbeddingModel,
     val reranker: Boolean,
-)
+) {
+    companion object {
+        fun toIndexDetailResponse(index: Index): IndexDetailResponse {
+            return IndexDetailResponse(
+                indexId = index.id,
+                chunkingSize = index.chunkingSize,
+                overlapSize = index.overlapSize,
+                similarityMetric = index.similarityMetric,
+                topK = index.topK,
+                embeddingModel = index.embeddingModel,
+                reranker = index.reranker,
+                snapshotName = index.snapshotName,
+            )
+        }
+    }
+}
